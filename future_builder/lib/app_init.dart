beb902f6b19eae4252cbfb6e5e2337ff0244eab2
Iara Lopes <iarasiria@gmail.com>
2021-01-27 17:20:03 -0300

    First steps

---
+++ future_builder/lib/app_init.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:future_builder/app_screen.dart';
+
+class AppInit extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppScreen();
+  }
+}
